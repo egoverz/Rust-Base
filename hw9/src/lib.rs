a32c4efba6b5b87bc1635a323bce76cbcdbcd589
egorya <yegorpereverzev@gmail.com>
2026-06-17 13:53:29 +0200

    add hw9

--- hw9/src/lib.rs
+++ hw9/src/lib.rs
@@ -1,4 +1,5 @@
 #![deny(unsafe_code)]
+use crate::sync::{Arc, Condvar, Mutex, thread};
 
 #[cfg(feature = "loom")]
 pub mod sync {
@@ -13,9 +14,24 @@ pub mod sync {
 }
 
 pub type Task = fn(i64);
+struct Shared {
+    state: Mutex<State>,
+    has_work: Condvar,
+}
+
+struct State {
+    queue: Vec<TaskItem>,
+    shutting_down: bool,
+}
+
+struct TaskItem {
+    num: i64,
+}
 
 pub struct ThreadPool {
-    _private: (),
+    shared: Arc<Shared>,
+    workers: Vec<thread::JoinHandle<()>>,
+    task: Task,
 }
 
 impl ThreadPool {
@@ -25,18 +41,77 @@ impl ThreadPool {
     ///
     /// Should panic when `worker_count == 0`.
     pub fn new(worker_count: usize, task: Task) -> Self {
-        let _ = (worker_count, task);
-        todo!("create shared queue, spawn workers, and return ThreadPool")
+        if worker_count == 0 {
+            panic!("worker_count must > 0");
+        }
+
+        let state = State {
+            queue: Vec::new(),
+            shutting_down: false,
+        };
+
+        let shared = Arc::new(Shared {
+            state: Mutex::new(state),
+            has_work: Condvar::new(),
+        });
+
+        let mut handles = Vec::new();
+        for i in 0..worker_count {
+            let shared_clone = shared.clone();
+            let handle = thread::spawn(move || {
+                loop {
+                    let maybe_num = {
+                        let mut state = shared_clone.state.lock().expect("mutex poisoned");
+                        while state.queue.is_empty() && !state.shutting_down {
+                            state = shared_clone.has_work.wait(state).expect("mutex poisoned");
+                        }
+
+                        if let Some(num) = state.queue.pop() {
+                            Some(num)
+                        } else if state.shutting_down {
+                            None
+                        } else {
+                            continue;
+                        }
+                    };
+
+                    match maybe_num {
+                        Some(num) => task(num.num),
+                        None => break,
+                    }
+                }
+            });
+            handles.push(handle);
+        }
+
+        ThreadPool {
+            shared,
+            workers: handles,
+            task,
+        }
+
+        // let _ = (worker_count, task);
+        // todo!("create shared queue, spawn workers, and return ThreadPool")
     }
 
     /// Add one number to the work queue.
     pub fn execute(&self, num: i64) {
-        let _ = num;
-        todo!("push a task argument into the queue and notify one worker")
+        let mut state = self.shared.state.lock().expect("mutex poisoned");
+        state.queue.push(TaskItem { num });
+        self.shared.has_work.notify_one();
     }
 
     /// Finish all queued work and stop all workers.
     pub fn shutdown(self) {
-        todo!("set shutdown flag, notify all workers, and join them")
+        {
+            let mut state = self.shared.state.lock().expect("mutex poisoned");
+            state.shutting_down = true;
+            self.shared.has_work.notify_all();
+        }
+
+        for worker in self.workers {
+            worker.join().expect("worked panicked");
+        }
+
     }
 }
